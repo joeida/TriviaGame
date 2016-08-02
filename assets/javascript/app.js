384e4f40ff68ca1b48e15733c087d67d08bba0d9
joe <joeida@jpl.nasa.gov>
2016-08-01 18:31:05 -0700

    Added gif for wrong answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,69 +3,78 @@ var trivia = [
         question: 'Who is known as the Black Mamba?',
         correct: 'Kobe Bryant',
         answer: ['Michael Jordan', 'Kobe Bryant', 'Magic Johnson', 'Allen Iverson'],
-        query: 'kobe+bryant+black+mamba',
-        imageIndex: 10
+        // query: 'kobe+bryant+black+mamba',
+        // imageIndex: 9,
+        gif: "https://media3.giphy.com/media/1w9UOyHVcBt5u/200.gif" 
     },
     {
         question: 'Who is the NBA all time scoring leader?',
         correct: 'Kareem Abdul Jabbar',
         answer: ['Jerry West', 'Michael Jordan', 'Wilt Chaimberlain', 'Kareem Abdul Jabbar'],
-        query: 'kareem+abdul+jabbar',
-        imageIndex: 4
+        // query: 'kareem+abdul+jabbar',
+        // imageIndex: 4,
+        gif: "http://media2.giphy.com/media/vTUqXBbRhVRSM/200.gif"
     },
     {
         question: 'Which team won the 2009 NBA championship?',
         correct: 'Los Angeles Lakers',
         answer: ['Los Angeles Lakers', 'Chicago Bulls', 'Portland Trailblazers', 'San Antonio Spurs'],
-        query: 'los+angeles+lakers+2009',
-        imageIndex: 4
+        // query: 'los+angeles+lakers+2009',
+        // imageIndex: 2,
+        gif: "http://media1.giphy.com/media/6nkc7Q4fTVZu0/200.gif"
     },
     {
         question: 'Which country won the first olympic gold medal in basketball?',
         correct: 'USA',
         answer: ['Russia', 'Germany', 'USA', 'Spain'],
-        query: 'michael+jordan+olympic',
-        imageIndex: 0
+        // query: 'michael+jordan+olympic',
+        // imageIndex: 0,
+        gif: "http://media1.giphy.com/media/lve8yY5NwwUJq/200.gif"
     },
     {
-        question: 'Who was the NBA Championship trophy named after?',
+        question: 'Who was the NBA logo imaged after?',
         correct: 'Jerry West',
         answer: ['Michael Jordan', 'Wilt Chaimberlain', 'Bill Russel', 'Jerry West'],
-        query: 'jerry+west',
-        imageIndex: 6
+        // query: 'jerry+west',
+        // imageIndex: 6,
+        gif: "http://media0.giphy.com/media/ySsvSqqvAJWvu/200.gif"
     },
     {
         question: 'Who holds the record for most points in a single NBA game?',
         correct: 'Wilt Chaimberlain',
         answer: ['Larry Bird', 'Michael Jordan', 'Wilt Chaimberlain', 'Kobe Bryant'],
-        query: 'wilt+chaimberlain',
-        imageIndex: 8
+        // query: 'wilt+chaimberlain',
+        // imageIndex: 10,
+        gif: "http://media0.giphy.com/media/BwFoijAZtoUxO/200.gif"
     },
     {
         question: 'How many teams are in the NBA?',
         correct: '30',
         answer: ['15', '25', '30', '35'],
-        query: 'nba+teams',
-        imageIndex: 1
+        // query: 'nba+teams',
+        // imageIndex: 1,
+        gif: "http://media0.giphy.com/media/u30NotdkEajGo/200.gif"
     },
     {
         question: 'Which team has the record amount of NBA Championships?',
         correct: 'Boston Celtics',
         answer: ['Los Angeles Lakers', 'Boston Celtics', 'Chicago Bulls', 'San Antonio Spurs'],
-        query: 'nba+boston+celtic',
-        imageIndex: 2
+        // query: 'nba+boston+celtic',
+        // imageIndex: 2,
+        gif: "http://media0.giphy.com/media/12ZVSHKrVcckfe/200.gif"
     },
     {
         question: 'What is the name of the Championship trophy given to the team who wins the NBA finals?',
         correct: "Larry O'Brien Championship Trophy",
         answer: ['NBA Finals Championship Trophy', 'Bill Russel Championship Trophy', 'McDonalds Championship Trophy', "Larry O'Brien Championship Trophy"],
-        query: 'larry+obrien+trophy',
-        imageIndex: 2
+        // query: 'larry+obrien+trophy',
+        // imageIndex: 2,
+        gif: "http://media4.giphy.com/media/yo6ntHXOpVIqY/200.gif"
     }
 ];
 
 var compute = {
-    questionNumber: 8,
+    questionNumber: 0,
     gameTime: 90,
     questionTime: 30,
     betweenTime: 2,
@@ -212,25 +221,35 @@ var output = {
     correct: function() {
         $('#triviaDiv').html('<div>' + output.winOutput + '</div>');
         $('#timerDiv').css('display', 'none');
-        console.log(compute.questionNumber);
         output.gifAnimate();
     },
 
     // Display Correct Answer Gif
     gifAnimate: function() {
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + trivia[compute.questionNumber].query + "&api_key=dc6zaTOxFJmzC";
-        var imageIndex = trivia[compute.questionNumber].imageIndex; 
-        $.ajax({url: queryURL, method: 'GET'})
-        .done(function(response) {
-            console.log(response);
-            $('#triviaDiv').append('<img src=' + response.data[imageIndex].images.fixed_height.url + ' >');
-        });
+        // var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + trivia[compute.questionNumber].query + "&api_key=dc6zaTOxFJmzC";
+        // var imageIndex = trivia[compute.questionNumber].imageIndex; 
+        // $.ajax({url: queryURL, method: 'GET'})
+        // .done(function(response) {
+        //     console.log(response);
+        //     $('#triviaDiv').append('<img src=' + response.data[imageIndex].images.fixed_height.url + ' >');
+        // });
+        var imageGif = trivia[compute.questionNumber].gif; 
+        $('#triviaDiv').append('<img src=' + imageGif +  ' >');
     },
 
     // Output trivia wrong answer chosen text and output correct answer
     wrong: function() {
+        // var queryURL = "http://api.giphy.com/v1/gifs/search?q=wrong&api_key=dc6zaTOxFJmzC";
+        // var imageIndex = 12;
+        // $.ajax({url: queryURL, method: 'GET'})
+        // .done(function(response) {
+        //     console.log(response);
+        //     $('#triviaDiv').append('<img src=' + response.data[imageIndex].images.fixed_height.url + ' >');
+        // });
         $('#triviaDiv').html('<div>' + output.loseOutput + ' The correct answer is ' + trivia[compute.questionNumber].correct + '.</div>');
         $('#timerDiv').css('display', 'none');
+        var imageGif = "http://media1.giphy.com/media/33bpFN25l6qNW/200.gif";
+        $('#triviaDiv').append('<img src=' + imageGif +  ' >');
     },
 
     // Output trivia unanswered text and output correct answer
